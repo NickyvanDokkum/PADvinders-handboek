ec99ce9047f74fad1fde220061368006b8df935d
Laurens <Laurenscnossen@gmail.com>
2020-03-27 12:51:04 +0100

    kaart stats

--- Moo Moo Meadows/Assets/Scripts/ClickBig.cs
+++ Moo Moo Meadows/Assets/Scripts/ClickBig.cs
@@ -3,18 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class ClickBig : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+public class ClickBig : MonoBehaviour, IPointerClickHandler
 {
-    public void OnPointerDown(PointerEventData eventData)
+    public void OnPointerClick(PointerEventData eventData)
     {
-        Debug.Log(this.transform.lossyScale);
-        this.transform.localScale = new Vector3(3, 3);
-
+        this.transform.localScale = new Vector2(3, 3);
+       // this.transform.position = new Vector2(Screen.width / 2, Screen.height / 2);
     }
+    //public void OnPointerDown(PointerEventData eventData)
+    //{
+    //    Debug.Log(this.transform.lossyScale);
+    //    this.transform.localScale = new Vector3(3, 3);
 
-    public void OnPointerUp(PointerEventData eventData)
-    {
-        Debug.Log(this.transform.lossyScale);
-        this.transform.localScale = new Vector3(1, 1);
-    }
+    //}
+
+    //public void OnPointerUp(PointerEventData eventData)
+    //{
+    //    Debug.Log(this.transform.lossyScale);
+    //    this.transform.localScale = new Vector3(1, 1);
+    //}
 }
--- Moo Moo Meadows/Assets/Scripts/DragDrop.cs
+++ Moo Moo Meadows/Assets/Scripts/DragDrop.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class DragDrop : MonoBehaviour, IBeginDragHandler, IDragHandler,IEndDragHandler
+public class DragDrop : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     public Transform ReturnParent = null;
     public void OnBeginDrag(PointerEventData eventData)
