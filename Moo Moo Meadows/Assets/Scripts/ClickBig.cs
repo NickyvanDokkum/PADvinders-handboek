5892ddb0c861388f84a55500e9daad5801e79b81
Laurens <Laurenscnossen@gmail.com>
2020-03-30 15:06:11 +0200

    comments, redundancy en aparte scene

--- Moo Moo Meadows/Assets/Scripts/ClickBig.cs
+++ Moo Moo Meadows/Assets/Scripts/ClickBig.cs
@@ -9,14 +9,15 @@ public class ClickBig : MonoBehaviour,IPointerEnterHandler, IPointerExitHandler
     GameObject gezoomdeKaart;
     [SerializeField] public Transform hands;
 
-    public void OnPointerExit(PointerEventData eventData)
-    {
-        Destroy(gezoomdeKaart);
-    }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        gezoomdeKaart = Instantiate(gameObject, new Vector2(Screen.width / 2, Screen.height / 2), transform.rotation, hands);
-        gezoomdeKaart.transform.localScale = new Vector2(2, 2);
+        gezoomdeKaart = Instantiate(gameObject, new Vector2(Screen.width / 2, Screen.height / 2), transform.rotation, hands);   //maakt een cloon van de kaart waar overheen wordt gehovert en zet zijn positie naar het midden van het scherm
+        gezoomdeKaart.transform.localScale = new Vector2(2, 2);     //vergroot de geclonede kaart en tekst zodat het goed leesbaar is
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Destroy(gezoomdeKaart);     //verwijdert de geclonede kaart wanneer je niet meer over de basiskaart hovert
     }
 }
