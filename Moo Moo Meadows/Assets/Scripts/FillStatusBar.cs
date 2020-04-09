d07edc69ccda6577f07c721e80a4eb75964277ca
bossie911 <62554007+bossie911@users.noreply.github.com>
2020-04-09 13:54:29 +0200

    Grades Bar

---
+++ Moo Moo Meadows/Assets/Scripts/FillStatusBar.cs
@@ -0,0 +1,35 @@
+﻿       using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FillStatusBar: MonoBehaviour
+{
+    public float grades;
+    public float maxStats;
+    public Image fillimage;
+    private Slider slider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider = GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (slider.value <= slider.minValue)
+        {
+            fillimage.enabled = false;
+        }
+
+        if (slider.value > slider.minValue && !fillimage.enabled)
+        {
+            fillimage.enabled = true;
+        }
+
+        float fillValue = grades / maxStats;
+        slider.value = fillValue; 
+    }
+}
