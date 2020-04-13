08803a068ba7dea433cb2ee5c6c37b76be7490f1
J. Riedeman <j.riedeman@hva.nl>
2020-04-13 22:10:30 +0200

    alles getest en werkt

--- Moo Moo Meadows/Assets/Scripts/Time_system/Calendar.cs
+++ Moo Moo Meadows/Assets/Scripts/Time_system/Calendar.cs
@@ -2,49 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
-using UnityEngine.UI;
-
 public class Calendar : MonoBehaviour
 {
     List<dayInformation> plannedDays;
     int currentDay;
-
-
-    Text txt;
-
 
     // Start is called before the first frame update
     void Start()
     {
         currentDay = 1;
         plannedDays = new List<dayInformation>();
-
-
-        txt = transform.Find("Text").GetComponent<Text>();
-
-        txt.text = "hij is gestart";
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 
     public void advanceDay()
     //elke dag is er ook nog een structured event maar daar hoef ik niks mee te doen
     {
-        txt.text = "hij gaat nu over naar de volgende dag";
 
         currentDay++;
-
-        txt.text = "de dag is nu" + currentDay;
 
         //check of de week is afgelopen om vervolgens de huur te betalen
         if (currentDay / 8 == currentDay % 8)
         {
-            txt.text = "er is een week voorbij";
             //betaal de huur
 
             //verwijder de dingen uit de list van de vorige week
@@ -67,11 +45,15 @@ public class Calendar : MonoBehaviour
             if (plannedDay.day == currentDay)
             {
                 //begin het event
-                txt.text = "er is een event";
 
                 break;
             }
         }
+    }
+
+    public void planEvent(int day)
+    {
+        plannedDays.Add(new dayInformation(day));
     }
 
     class dayInformation
