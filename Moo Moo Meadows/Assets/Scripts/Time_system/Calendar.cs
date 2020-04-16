d2ae7d3e52e1408256891e7c55c037de90a277ca
J. Riedeman <j.riedeman@hva.nl>
2020-04-16 14:37:19 +0200

    added some test data

---
+++ Moo Moo Meadows/Assets/Scripts/Time_system/Calendar.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Calendar : MonoBehaviour
+{
+    List<DayInformation> plannedDays;
+    int currentDay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentDay = 1;
+        plannedDays = new List<DayInformation>();
+    }
+
+    public void advanceDay()
+    //elke dag is er ook nog een structured event maar daar hoef ik niks mee te doen
+    {
+
+        currentDay++;
+
+        //check of de week is afgelopen om vervolgens de huur te betalen
+        if (currentDay / 8 == currentDay % 8)
+        {
+            //betaal de huur
+
+            //verwijder de dingen uit de list van de vorige week
+            for (int index = plannedDays.Count - 1; index >= 0; index--)
+            {
+                DayInformation plannedDay = plannedDays[index];
+
+                if (plannedDay.day < currentDay)
+                {
+                    plannedDays.Remove(plannedDay);
+
+                }
+            }
+
+        }
+
+        //check of er event zijn ingepland voor de volgende dag
+        foreach (DayInformation plannedDay in plannedDays)
+        {
+            if (plannedDay.day == currentDay)
+            {
+                //begin het event
+                break;
+            }
+            else
+            {
+                // er is geen event geplanned voor vandaag
+            }
+        }
+    }
+
+    //nog niet getest
+    public List<DayInformation> getEventsForFourWeeks()
+    {
+        List<DayInformation> events = new List<DayInformation>();
+
+        //hoeveel dagen er in 4 weken zitten plus 1
+        int days = 29;
+
+        if(days > plannedDays.Count())
+        {
+            days = plannedDays.Count();
+        }
+
+        for (int i = 0; index < plannedDays; index++)
+        {
+            events.Add(plannedDays[index]);
+        }
+
+        return events;
+    }
+
+    public void planEvent(int day, test_event_data cardEvent)
+    {
+        plannedDays.Add(new DayInformation(day, cardEvent));
+    }
+
+    class DayInformation
+    {
+        public int day;
+        test_event_data cardEvent;
+
+        // hier moet ook het event worden opgeslagen
+
+        public DayInformation(int day, test_event_data cardEvent)
+        {
+            this.day = day;
+            this.cardEvent = cardEvent;
+        }
+    }
+
+    // dit is alleen om te testen
+    public void fillMonth(int startWeek)
+    {
+        test_event_data cardEvent = new test_event_data();
+
+        for (int index = 0; indexndex < 28; index++)
+        {
+            planEvent(startWeek + index, cardEvent);
+        }
+    }
+}
---
+++ Moo Moo Meadows/Assets/Scripts/Time_system/test_event_data.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test_event_data : MonoBehaviour
+{
+
+    public string title;
+    public string body;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        title = "event 1";
+        body = "dit is event 1...";
+    }
+}
